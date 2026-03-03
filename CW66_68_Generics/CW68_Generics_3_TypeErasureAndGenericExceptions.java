653470d284bac360a640eace763c417ff8e46186
gupavi <abhinav200002@gmail.com>
2026-03-04 05:06:57 +0530

    Moved generics code in subfolder

--- CW66_68_Generics/CW68_Generics_3_TypeErasureAndGenericExceptions.java
+++ CW66_68_Generics/CW68_Generics_3_TypeErasureAndGenericExceptions.java
@@ -1,7 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class CW68_Generics_3_TypeErasureAndGenericExceptions {
     public static void main(String[] args) {
         /* Type Erasure */

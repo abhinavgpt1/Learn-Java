395c6cc7585f8370a610ac351d73fc91be96cd71
abhinavgpt1 <abhinav200002@gmail.com>
2026-03-23 02:53:17 +0530

    HW14: improved docs for program to recursively find count of files and folder for a given directory path

--- HW/HW14_GetFolderStructureFullInfo.java
+++ HW/HW14_GetFolderStructureFullInfo.java
@@ -2,34 +2,31 @@ import java.io.File;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 
 // Make sure there exist no .class in HW folder (to match output) -> else execute "del /S *.class"
 
 public class HW14_GetFolderStructureFullInfo {
 	public static void main(String[] args) throws IOException {
 		// Can take input from user for folder path
-			// Scanner cin = new Scanner(System.in);
-			// System.out.print("Input canonical folder path: ");
-			// String folderPath = cin.nextLine();
-			// cin.close();
-			File root_folder = new File(".");
-			// File root_folder = new File("..\\Technicalities"); //example 2 - there are a lot of getName() output with same name eg. pack
+		// Scanner cin = new Scanner(System.in);
+		// System.out.print("Input canonical folder path: ");
+		// String folderPath = cin.nextLine();
+		// cin.close();
+		File root_folder = new File(".");
+		// File root_folder = new File("..\\Technicalities"); // example 2 - there are a lot of getName() output with same name eg. pack
 		if (!root_folder.isDirectory()) {
 			System.out.println("Path provided is not of directory.");
 			return;
 		}
 		System.out.println("Directory structure for: " + root_folder.getCanonicalPath());
-		System.out.println("--------------------------------------------------");
+		System.out.println("-------------------------------------------------------------");
 
 		// BFS to get info of all files and folders in the directory
 		Queue<File> pending = new LinkedList<>();
 		pending.add(root_folder);
 
 		while (!pending.isEmpty()) {
-			File folder = pending.peek();
-			pending.remove();
-			// use pending.poll() for return and pop
+			File folder = pending.remove();
 
 			System.out.println("For " + folder.getName() + ":"); // suggestion for example 2: for proper directory path, maintain parent path in Queue<Pair<File, String>>
 			int fileCount = 0, folderCount = 0;
@@ -39,7 +36,7 @@ public class HW14_GetFolderStructureFullInfo {
 					pending.add(file);
 					folderCount++;
 				} else {
-					// System.out.println("\t" + file.getName()); //considers .class if File root_folder = new File(".");
+					// System.out.println("\t" + file.getName()); // considers HW14.class as file if File root_folder = new File(".");
 					fileCount++;
 				}
 			}
@@ -47,20 +44,29 @@ public class HW14_GetFolderStructureFullInfo {
 			System.out.println("\t folders: " + folderCount);
 		}
 	}
-
-	/**
-	 * Output:
-	 * -------
-	 * Directory structure for: W:\BCE (C,Cpp,Java,WebDev)\Java\core-java.git\HW
-	 * --------------------------------------------------
-	 * For .:
-	 * 	 files: 15
-	 * 	 folders: 1
-	 * For HW13_CountFilesWithDifferentExtension:
-	 * 	 files: 4
-	 * 	 folders: 1
-	 * For Dir1:
-	 * 	 files: 1
-	 * 	 folders: 0
-	 */
 }
+
+/**
+ * Output:
+ * -------
+ * Directory structure for: V:\BCE Coding\Java\learn-java.git\HW
+ * -------------------------------------------------------------
+ * For .:
+ * 	 files: 16
+ * 	 folders: 4
+ * For HW13_CountFilesWithDifferentExtension:
+ * 	 files: 4
+ * 	 folders: 1
+ * For HW15_CopyAndCreateImage:
+ * 	 files: 2
+ * 	 folders: 0
+ * For HW16_CutAndMergeRingtones:
+ * 	 files: 4
+ * 	 folders: 0
+ * For HW17_DataInputStreamDataOutputStream:
+ * 	 files: 2
+ * 	 folders: 0
+ * For Dir1:
+ * 	 files: 1
+ * 	 folders: 0
+ */

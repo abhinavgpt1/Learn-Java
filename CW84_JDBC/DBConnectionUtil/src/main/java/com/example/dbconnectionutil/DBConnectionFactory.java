713fa404012d1bf0f4bc1f3fa92771ac75c5a9db
abhinavgpt1 <abhinav200002@gmail.com>
2026-03-31 20:52:24 +0530

    CW84: added generic database connection util

---
+++ CW84_JDBC/DBConnectionUtil/src/main/java/com/example/dbconnectionutil/DBConnectionFactory.java
@@ -0,0 +1,22 @@
+package CW84_JDBC.DBConnectionUtil.src.main.java.com.example.dbconnectionutil;
+
+import com.example.dbconnectionutil.dbconnection.MySQLConnection;
+import com.example.dbconnectionutil.dbconnection.PostgreSQLConnection;
+import com.example.dbconnectionutil.dbconnection.SQLServerConnection;
+
+import java.sql.Connection;
+
+public class DBConnectionFactory {
+    public static Connection getConnection(Database database) {
+        switch (database) {
+            case MYSQL:
+                return MySQLConnection.getConnection();
+            case POSTGRES:
+                return PostgreSQLConnection.getConnection();
+            case SQLSERVER:
+                return SQLServerConnection.getConnection();
+            default:
+                throw new IllegalArgumentException("Unknown database: " + database);
+        }
+    }
+}
---
+++ CW84_JDBC/DBConnectionUtil/src/main/java/com/example/dbconnectionutil/Database.java
@@ -0,0 +1,5 @@
+package CW84_JDBC.DBConnectionUtil.src.main.java.com.example.dbconnectionutil;
+
+public enum Database {
+    MYSQL, POSTGRES, SQLSERVER
+}

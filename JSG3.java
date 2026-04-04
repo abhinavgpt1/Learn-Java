cc7464c4d7a9d43392418c9e2cf8427e83bfce4c
abhinavgpt1 <abhinav200002@gmail.com>
2026-04-04 06:57:02 +0530

    CW3: long datatype representation and boxing/unboxing note

--- JSG3.java
+++ JSG3.java
@@ -10,7 +10,27 @@ class JSG3 {
 		System.out.println(c + 3);
 		float f = (float) (3.14 * 10 * 10);
 		System.out.println(f);
-		// float q=1.9; erroneous since, implict narrowing is not possible (double ->
-		// float)
+		// float q=1.9; erroneous since, implict narrowing is not possible (double -> float)
+
+		long l1 = 89L;
+		long l2 = 89l;
+		long l3 = 89; // Widening. 89 is by default int.
+		if (l1 == l2 && l2 == l3) {
+			System.out.println("All long representations are equal");
+			System.out.println("Representation with L is recommended since 89l looks like 891");
+		}
+		// Narrowing / Widening -> Casting between primitive data types
+		// != 
+		// Autoboxing (Boxing) / Unboxing -> Wrapper classes
 	}
 }
+
+/**
+ * Output:
+ * -------
+ * 5
+ * 5.0
+ * 314.0
+ * All long representations are equal
+ * Representation with L is recommended since 89l looks like 891
+ */
